f4913f0c45ba308cd1a7a2221454a38f15a7b75f
Leslie Young <pl.young@gmail.com>
2020-09-08 19:47:05 +0200

    1.0.1
    More restructuring.

---
+++ Assets/game-settings-sample/Scripts/Sample.cs
@@ -0,0 +1,34 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Game
+{
+	public class Sample : MonoBehaviour
+	{
+		[SerializeField] private AudioSource testSound=null;
+		[SerializeField] private TMP_Text resolutionLabel=null;
+
+		private void Start()
+		{
+			// initialize and restore game settings as soon as game started
+			GameSettingsManager.RestoreSettings();
+		}
+
+		private void LateUpdate()
+		{
+			resolutionLabel.text = $"{Screen.currentResolution} {Screen.fullScreenMode} ({Screen.width} x {Screen.height} {(Screen.fullScreen ? "Fullscreen" : "Window")})";
+		}
+
+		public void PlayTestSound()
+		{
+			testSound.Play();
+		}
+
+		public void QuitGame()
+		{
+			Application.Quit();
+		}
+
+		// ------------------------------------------------------------------------------------------------------------
+	}
+}
